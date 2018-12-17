15e30a880f877df343b1d920d7ec87ddf0515d03
Bill Qiu <fanglaiq@gmail.com>
2018-12-17 11:20:02 +0800

    umi

--- panel/.umirc.js
+++ panel/.umirc.js
@@ -1,18 +1,20 @@
-
 // ref: https://umijs.org/config/
 export default {
-  plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: false,
-      dva: true,
-      dynamicImport: false,
-      title: 'panel',
-      dll: false,
-      routes: {
-        exclude: [],
-      },
-      hardSource: false,
-    }],
-  ],
-}
+    plugins: [
+        // ref: https://umijs.org/plugin/umi-plugin-react.html
+        [
+            'umi-plugin-react',
+            {
+                antd: false,
+                dva: true,
+                dynamicImport: false,
+                title: 'panel',
+                dll: false,
+                routes: {
+                    exclude: [],
+                },
+                hardSource: false,
+            },
+        ],
+    ],
+};
